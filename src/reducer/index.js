0ddf68c031149b1cb69538a47f65681e9532f531
rka87338 <rka87338@gmail.com>
2019-08-06 03:43:06 +0530

    included spread operater in reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -5,15 +5,17 @@ const initialState = {
 function reducer(state = initialState, action) {
   switch (action.type) {
     case 'GREET_REACT':
-      console.log('From Reducer: ', action.PAYLOAD.tech);
+      ...state, // reducer will overright the complete state, what if there were more than one key-value pairs, all that would be over written by one thing(newly returned one). so we need to first copy all of them and later over write the update thing
       return Object.assign({}, state, {
         receiver: 'React JS'
       });
     case 'GREET_REDUX':
+	...state,
       return Object.assign({}, state, {
         receiver: 'Redux'
       });
     case 'GREET_BOTH':
+	...state,
       return Object.assign({}, state, {
         receiver: 'REACT AND REDUX'
       });
